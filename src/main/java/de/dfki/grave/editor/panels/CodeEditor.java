3f096c0deefbe8713ca854f35215bb641a89bf9a
Bernd Kiefer <j_a_w_bone@gmx.de>
2020-06-01 19:30:20 +0200

    A bit of cleanup.

--- src/main/java/de/dfki/grave/editor/panels/CodeEditor.java
+++ src/main/java/de/dfki/grave/editor/panels/CodeEditor.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.plaf.basic.BasicSplitPaneDivider;
 import javax.swing.plaf.basic.BasicSplitPaneUI;
 
 import org.fife.ui.rsyntaxtextarea.RSyntaxDocument;
@@ -38,54 +40,70 @@ public class CodeEditor extends JPanel {
     b.setMinimumSize(bDim);
     b.setMaximumSize(bDim);
     b.setPreferredSize(bDim);
-    //b.setOpaque(false);
-
-    //b.setContentAreaFilled(false);
-    //b.setFocusable(false);
     b.setBorder(BorderFactory.createEmptyBorder());
   }
-
+  
+  /**
+   * Makes a split pane invisible. Only contained components are shown.
+   *
+   * @param splitPane
+   */
+  public static void flattenJSplitPane(JSplitPane splitPane) {
+      splitPane.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
+      BasicSplitPaneUI flatDividerSplitPaneUI = new BasicSplitPaneUI() {
+          @Override
+          public BasicSplitPaneDivider createDefaultDivider() {
+              return new BasicSplitPaneDivider(this) {
+                  @Override
+                  public void setBorder(Border b) {
+                  }
+              };
+          }
+      };
+      splitPane.setUI(flatDividerSplitPaneUI);
+      splitPane.setBorder(null);
+  }
+  
   public CodeEditor(final EditorProject project) {
     mEditorProject = project;
-
     setLayout(new BorderLayout());
+
     splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, true);
     splitPane.setResizeWeight(1.0);
-
+    splitPane.setBorder(BorderFactory.createEmptyBorder());
     splitPane.setUI(new BasicSplitPaneUI());
     splitPane.setDividerSize(10);
-    splitPane.setDividerLocation(400);
-
+    splitPane.setDividerLocation(.5);
+    flattenJSplitPane(splitPane);
+    
     mRightTextArea = new EditCodeArea(
               mEditorProject.getEditorConfig().sCODE_DIVIDER_LOCATION);
+    mRightTextArea.setBorder(BorderFactory.createLineBorder(Color.gray));
     mLeftTextArea = new EditCodeArea(
               mEditorProject.getEditorConfig().sCODE_DIVIDER_LOCATION);
-
+    mLeftTextArea.setBorder(BorderFactory.createLineBorder(Color.gray));
+    
     mPinButton = new JButton();
     mPinButton.setVisible(true);
     pinPricked = mEditorProject.getEditorConfig().sAUTOHIDE_BOTTOMPANEL;
     setPin(pinPricked);
     mPinButton.setContentAreaFilled(false);
-    //mPinButton.setMargin(new Insets(0, 10, 20, 10));
     mPinButton.setFocusable(false);
     mPinButton.addActionListener((ActionEvent e) -> { setPin(!pinPricked); });
     sanitizeTinyButton(mPinButton);
     Box VpinBox = Box.createVerticalBox();
     VpinBox.add(mPinButton);
     VpinBox.add(Box.createVerticalGlue());
-    //add(Box.createHorizontalGlue());
     add(VpinBox, BorderLayout.EAST);
 
     splitPane.setRightComponent(mRightTextArea);
     splitPane.setLeftComponent(mLeftTextArea);
-    //jsp.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
     add(splitPane, BorderLayout.CENTER);
   }
 
   private class EditCodeArea extends JPanel {
     RSyntaxTextArea textArea;
     EditorComponent editedObject;
-    JScrollPane s;
 
     // make this jpane, with scrollpane, and textarea
     public EditCodeArea(int dividerLocation) {
@@ -94,14 +112,12 @@ public class CodeEditor extends JPanel {
       textArea.setCodeFoldingEnabled(true);
       textArea.setLineWrap(true);
       textArea.setWrapStyleWord(true);
-      //textArea.setBounds(this.getBounds());
-      //setBounds(0, 0, 5000, dividerLocation);
       textArea.setVisible(true);
 
       // Get rid of annoying yellow line
       textArea.setHighlightCurrentLine(false);
       textArea.setHighlightSecondaryLanguages(false);
-      s = new JScrollPane();
+      JScrollPane s = new JScrollPane();
       s.add(textArea);
       add(s, BorderLayout.CENTER);
     }
